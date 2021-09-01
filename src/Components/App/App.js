b8a7f2feb50bd9b5eafe22fad932fda2ca9032aa
asiedusolo <solomon.asiedu@amalitech.org>
2021-09-01 15:04:05 -0400

    adding extra css

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import "./SearchResults.css";
 
 function SearchResults() {
