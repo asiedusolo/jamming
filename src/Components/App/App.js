0b39900ee2cc3c7b9f37322ba84bda88ddb5e4db
asiedusolo <solomon.asiedu@amalitech.org>
2021-09-01 16:11:15 -0400

    passing down search results from app to searchResults

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,22 +4,50 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        {/* Add a SearchBar component  */}
-        <SearchBar />
-        <div className="App-playlist">
-          {/* <!-- Add a SearchResults component --> */}
-          <SearchResults />
-          {/* <!-- Add a Playlist component --> */}
-          <Playlist />
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = { SearchResults: [
+      { id: 1,
+        name: 'Ayoo',
+        artist: 'Shatta Wale',
+        album: 'Reign'
+      },
+      { id: 2,
+        name: 'Rollies and Cigars',
+        artist: 'Sarkodie',
+        album: 'No Pressure'
+      },
+      { id: 3,
+        name: 'Nominate',
+        artist: 'Stonebwoy',
+        album: 'Anloga junction'
+      },
+      { id: 4,
+        name: 'My baby',
+        artist: 'Samini',
+        album: 'Dagati King'
+      },
+    ]}
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* Add a SearchBar component  */}
+          <SearchBar />
+          <div className="App-playlist">
+            {/* <!-- Add a SearchResults component --> */}
+            <SearchResults searchResults={this.state.searchResults}/>
+            {/* <!-- Add a Playlist component --> */}
+            <Playlist />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
